e27c0ced72c439efab5621d5824f9da280fafe1e
Mba Thomas <angelfab631@gmail.com>
2022-02-26 14:11:37 +0100

    Changes

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.getElementById('nav-toggle');
-const close = document.getElementById('close');
-
-
-// Toggle nav
-toggle.addEventListener('click', () => document.body.classList.add('show-nav'));
-
-// Hide modal
-close.addEventListener('click', () => document.body.classList.remove('show-nav'));

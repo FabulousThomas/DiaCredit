75aa5f197a4a6cb48dae198a4c604ba7343518f0
Mba Thomas <angelfab631@gmail.com>
2022-03-07 15:06:57 +0100

    Update
    Added an email funtion

--- js/action.php
+++ js/action.php
@@ -1,24 +1,16 @@
 <?php
-// FOOTER FORM -- SEND US A MESSAGE
-if(isset($_POST['send_msg'])) {
-    ini_set('SMTP', 'smtp.gmail.com');
-    ini_set('smtp_port', 25);
-    $mailto = "thomasangel697@gmail.com";
-    $from = $_POST['email']; //Sender's email
-    $name = $_POST['name']; //Sender's name
-    $sbj = $_POST['subject'];
-    $sbj2 = "We've received your message, and will get in touch with you shortly."; //Message to the client's email
-    $msg = $name ." sent the following message\n\n" . $_POST['message'];
-    $msg2 = "Dear " .$name .",\nThank you for contacting us at EB-Crediting";
-    $headers = "From: ". $from; //Sender's email address
-    $headers2 = "From: ". $mailto; //Sender's email address
-    $result = mail($mailto, $sbj2, $msg, $headers); //Sends email to owner
-    $result2 = mail($mailto, $sbj2, $msg2, $headers2); //Sends email to user
-    if($result) {
-        echo "<script>alert('Message sent successfully')</script>";
-    } else {
-        echo "<script>alert('Message failed\nPlease try again')</script>";
-    }
+
+$name = $_POST['name']; //Sender's name
+$email = $_POST['email']; //Sender's email
+$message = $_POST['message'];
+$to = "thomasangel697@gmail.com";
+$subject = "Mail from DiaCrediting.com";
+$txt = "Name = ".$name . "\r\n Email = " . $email . "\r\n Message = " . $message;
+$headers = "From: noreply@diacrediting.com" . "\r\n" . "CC: somebodyelse@example.com";
+
+if($email != null) {
+    mail($to, $subject, $txt, $headers);
 }
 
+header("Location: www.google.com");
 ?>

af154b769ea9cc37a02ab6d6ecd78da4fff170d7
Mba Thomas <angelfab631@gmail.com>
2022-03-21 01:15:32 +0100

    Changes made

--- php/action.php
+++ php/action.php
@@ -19,15 +19,16 @@ $name = $_POST['name']; //Sender's name
 $email = $_POST['email']; //Sender's email
 $message = $_POST['message'];
 $to = "thomasangel697@gmail.com";
-$subject = "Mail from DiaCrediting.com";
+$subject = "Mail from diaCrediting.com";
 $txt = "Name = ".$name . "\r\n Email = " . $email . "\r\n Message = " . $message;
 $headers = "From: noreply@diacrediting.com" . "\r\n" . "CC: somebodyelse@example.com";
 
 if($email != null) {
     mail($to, $subject, $txt, $headers);
-    echo "<script>alert('Message sent!')</script>";
+    header("Location: ../success.html");
+} else {
+    header("Location: ../error.html");
 }
 
-header("Location: https://www.google.com");
 
  ?>
